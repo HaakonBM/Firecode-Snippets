da627e8834d0cee9b7d23b5da74644ffd2650870
Haakon <wolfnr1@hotmail.com>
2020-03-01 00:28:13 +0100

    matrix horizontalflip

--- src/flipHorizontalAxis.java
+++ src/flipHorizontalAxis.java
@@ -1,4 +1,17 @@
 public class flipHorizontalAxis {
 
+    public static void flipHorizontalMatrix (int[][] matrix) {
+        int left = 0;
+        int right = matrix.length - 1;
 
+        while (left < right) {
+            swapArrays(matrix, left++, right--);
+        }
+    }
+
+    private static void swapArrays (int[][] matrix, int leftMatrix, int rightMatrix) {
+        int[] temp = matrix[leftMatrix];
+        matrix[leftMatrix] = matrix[rightMatrix];
+        matrix[rightMatrix] = temp;
+    }
 }
